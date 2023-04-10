696ff2134c0603b56f5826bd472fcadb2ee2a665
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-04-10 21:07:26 +0300

    #3080 - 100 points

--- src/verifnumar_3080.cpp
+++ src/verifnumar_3080.cpp
@@ -1,68 +1,95 @@
-// https://www.pbinfo.ro/probleme/3080/verifnumar
+// // https://www.pbinfo.ro/probleme/3080/verifnumar
 
-// Se dă un șir de caractere. Sa se determine numărul scris cu cifrele existente în șirul dat.
-// Să se afișeze numărul obținut și, pe linia următoare, toate caracterele din șir care nu fac parte din număr, cu excepția spațiilor.
+// // Se dă un șir de caractere. Sa se determine numărul scris cu cifrele existente în șirul dat.
+// // Să se afișeze numărul obținut și, pe linia următoare, toate caracterele din șir care nu fac parte din număr, cu excepția spațiilor.
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
+bool contains_digits(char s[])
+{
+    for (int i = 0; i < strlen(s); i++)
+        if ('0' <= s[i] && s[i] <= '9')
+            return true;
+    return false;
+}
+
+bool contains_only_zeros(char s[])
+{
+    bool ok = false;
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if ('1' <= s[i] && s[i] <= '9')
+            return false;
+        if (s[i] == '0')
+            ok = true;
+    }
+    return ok;
+}
+
 int main()
 {
-    char s[101], cif[101], lit[101];
-    strcpy(cif, "");
-    int ind_cif = 0, ind_lit = 0;
-    bool nan = true, zero = true, litere = true, natural = true;
+    char s[101], nr[101] = "", lit[101] = "";
+    int n = 0, l = 0;
     cin.getline(s, 101);
-    for (int i = 0; i < strlen(s); i++)
-        if (!strlen(cif))
-        {
-            if (strchr("123456789", s[i]))
-            {
-                cif[ind_cif] = s[i];
-                ind_cif++;
-                nan = false;
-                zero = false;
-            }
-            else
-            {
-                lit[ind_lit] = s[i];
-                ind_lit++;
-                litere = false;
-                if (s[i] == 0)
-                    natural = false;
-            }
-        }
-        else
-        {
-            if (strchr("0123456789", s[i]))
-            {
-                cif[ind_cif] = s[i];
-                ind_cif++;
-                nan = false;
-                if (s[i] != 0)
-                    zero = false;
-            }
-            else
-            {
-                lit[ind_lit] = s[i];
-                ind_lit++;
-                litere = false;
-            }
-        }
-    if (zero)
-        strcpy(cif, "0");
-    if (litere && natural)
-        cout << "CORECT";
-    else if (nan)
+    if (!contains_digits(s))
+    {
         cout << "NaN";
+        return 0;
+    }
+    if (contains_only_zeros(s))
+    {
+        int poz_fin_zero;
+        for (int i = 0; i < strlen(s); i++)
+            if (s[i] == '0')
+                poz_fin_zero = i;
+        cout << '0' << '\n';
+        for (int i = 0; i < strlen(s); i++)
+            if (i != poz_fin_zero && s[i] != ' ')
+                cout << s[i] << ' ';
+        return 0;
+    }
     else
     {
-        cout << cif << '\n';
-        for (int i = 0; i < strlen(lit); i++)
-            if (lit[i] == ' ')
-                continue;
-            else
-                cout << lit[i] << ' ';
+        for (int i = 0; i < strlen(s); i++)
+        {
+            if (s[i] == '0')
+            {
+                if (n > 0)
+                {
+                    nr[n] = s[i];
+                    n++;
+                    nr[n] = '\0';
+                }
+                else
+                {
+                    lit[l] = s[i];
+                    l++;
+                    lit[l] = '\0';
+                }
+            }
+            else if ('1' <= s[i] && s[i] <= '9')
+            {
+                nr[n] = s[i];
+                n++;
+                nr[n] = '\0';
+            }
+            else if ('a' <= s[i] && s[i] <= 'z')
+            {
+                lit[l] = s[i];
+                l++;
+                lit[l] = '\0';
+            }
+        }
     }
+    if (l == 0)
+        cout << "CORECT";
+    else
+    {
+        cout << nr << endl;
+        for (int i = 0; i < l; i++)
+            cout << lit[i] << ' ';
+    }
+    return 0;
 }
