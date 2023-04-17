2d03b1c392925d1d1b99196a88d9a42eefabaace
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-04-17 12:10:53 +0300

    #206 - 100 points

--- src/genmat1_206.cpp
+++ src/genmat1_206.cpp
@@ -1,3 +1,10 @@
+// https://www.pbinfo.ro/probleme/206/genmat1
+
+// Scrieţi un program care citeşte de la tastatură două numere naturale nenule n şi m
+// şi care construieşte în memorie şi apoi afişează o matrice A cu n linii
+// (numerotate de la 1 la n) şi m coloane (numerotate de la 1 la m) cu proprietatea că
+// fiecare element Aij memorează cea mai mică dintre valorile indicilor i şi j ( 1≤i≤n, 1≤j≤m ).
+
 #include <iostream>
 
 using namespace std;
