9809d4cf6b582b5c9f2375f81745c4f477e8f1de
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-04-24 21:02:46 +0300

    #2839 - Work in progress

---
+++ src/parimpar1_2839.cpp
@@ -0,0 +1,23 @@
+// https://www.pbinfo.ro/probleme/2839/parimpar1
+
+// Se dă un număr natural, n (n∈[2,5000]), și un șir de 2·n numere naturale
+// din intervalul [0,5]. Se cere să se determine valoarea obținută
+// însumând toate produsele de forma x·y, unde x și y sunt numere de
+// paritate diferită, x fiind printre primii n termeni ai șirului dat,
+// iar y printre ultimii n termeni ai acestui șir. Dacă nu există niciun
+// astfel de produs, valoarea cerută este nulă.
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+ifstream in("parimpar1.in");
+ofstream out("parimpar1.out");
+
+int main()
+{
+    int n;
+    in >> n;
+    // TODO
+}
