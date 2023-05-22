c25f9c99d257990d5f0f5996f21b339527a8c05c
Sarakuzoi <calin.andrei.radoi@gmail.com>
2023-05-22 19:40:27 +0300

    #234 - 100 points

--- src/medpoz_234.cpp
+++ src/medpoz_234.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -13,13 +14,27 @@ ofstream out("medpoz.out");
 
 int main()
 {
-    int n, a[21][21];
-    cin >> n;
+    int n, a[21][21], cnt = 0;
+    float s = 0;
+    in >> n;
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
-            cin >> a[i][j];
+            in >> a[i][j];
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= n; j++)
-            if (i <= j)
-    // TODO
+            if (i > j && a[i][j] > 0)
+            {
+                s += a[i][j];
+                cout << a[i][j] << endl;
+                cnt++;
+            }
+    if (cnt)
+    {
+        out << fixed << setprecision(3) << s / cnt;
+    }
+    else
+    {
+        out << "NU EXISTA";
+    }
+    return 0;
 }
